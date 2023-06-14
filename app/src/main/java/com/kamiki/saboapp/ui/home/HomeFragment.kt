6ca97afef284caf9c27e44c1c427c8f1c5b0f424
Rio Bintang Sadewa <Rio.Sadewa@tm.idu.ac.id>
2023-06-14 17:54:56 +0700

    New

--- app/src/main/java/com/kamiki/saboapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kamiki/saboapp/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.kamiki.saboapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.kamiki.saboapp.R
 import com.kamiki.saboapp.databinding.FragmentHomeBinding
+import com.kamiki.saboapp.ui.addcamera.AddCameraFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -34,5 +37,14 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.fab -> {
+                val intent = Intent(binding.root.context, AddCameraFragment::class.java )
+                v.context.startActivity(intent)
+            }
+        }
     }
 }
